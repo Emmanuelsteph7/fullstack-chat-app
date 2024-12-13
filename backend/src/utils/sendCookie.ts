d1ab5aea5669dd203eb5071da0ac863781c6e69f
Emmanuel Stephen <stephenemmanuel770@gmail.com>
2024-12-13 09:00:57 +0100

    added domain to the cookie config

--- backend/src/utils/sendCookie.ts
+++ backend/src/utils/sendCookie.ts
@@ -5,6 +5,7 @@ import {
   TOKEN_COOKIE_KEY,
   WEBSITE_ORIGIN,
 } from "../constants";
+import { removeProtocol } from "./removeProtocol";
 
 export const sendCookie = (
   res: Response,
@@ -12,12 +13,15 @@ export const sendCookie = (
   maxAge = DURATION_7_DAYS
 ) => {
   const isDevelopment = NODE_ENV === "development";
+  const formattedDomain = isDevelopment
+    ? "localhost"
+    : removeProtocol(WEBSITE_ORIGIN);
 
   res.cookie(TOKEN_COOKIE_KEY, token, {
     maxAge,
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: "none",
     secure: !isDevelopment,
-    domain: WEBSITE_ORIGIN,
+    domain: "localhost",
   });
 };
